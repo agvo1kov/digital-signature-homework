5fc0f6465d7675e6604fa75a43528a454f2e2f35
Alexander <agvolkov4@yandex.ru>
2019-05-17 23:59:41 +0300

    hashing added

---
+++ hash.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const crypto = require('crypto');
+fs.readFile('data.txt', function(err, buf) {
+    const content = buf.toString();
+    const hash = crypto.createHash('sha512');
+    const data = hash.update(content, 'utf-8');
+    const gen_hash = data.digest('hex');
+    fs.writeFile('result.txt', JSON.stringify({
+        'file': content,
+        'hash': gen_hash
+    }), (err) => {});
+});
